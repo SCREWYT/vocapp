934fa504490a6b5b66b8337c296021c5d50be98d
Rouvenbecker <rouven.rb.becker@googlemail.com>
2025-06-29 11:16:30 +0200

    Change HTML and CSS Style for a cleaner UI.

--- app.py
+++ app.py
@@ -380,3 +380,10 @@ def learn_set(set_id):
 # ----------------------------------
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
+    
+# ----------------------------------
+# Impressum
+# ----------------------------------
+@app.route('/imprint')
+def imprint():
+    return render_template('imprint.html')
